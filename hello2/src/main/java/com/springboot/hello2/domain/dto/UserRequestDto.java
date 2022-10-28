e4e557991e128e5142917f2805b04dfb910813c7
lucyoz <lucyoz.kwon@gmail.com>
2022-10-28 15:03:51 +0900

    UserRequestDto 수정

--- hello2/src/main/java/com/springboot/hello2/domain/dto/UserRequestDto.java
+++ hello2/src/main/java/com/springboot/hello2/domain/dto/UserRequestDto.java
@@ -1,4 +1,13 @@
 package com.springboot.hello2.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class UserRequestDto {
+    private String id;
+    private String name;
+    private String password;
+
 }
