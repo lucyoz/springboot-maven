45f98d6cfc815124e4b867493d76207081971663
lucyoz <lucyoz.kwon@gmail.com>
2022-10-28 14:21:52 +0900

    User, UserDao, UserController

---
+++ hello2/src/main/java/com/springboot/hello2/dao/UserDao.java
@@ -0,0 +1,60 @@
+package com.springboot.hello2.dao;
+
+import com.springboot.hello2.domain.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+
+@Component
+public class UserDao {
+
+    private final DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserDao(DataSource dataSource, JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.dataSource = dataSource;
+    }
+
+    public void add(User user) {
+        this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)",
+                user.getId(), user.getName(), user.getPassword());
+    }
+
+    public int deleteAll() {
+        return this.jdbcTemplate.update("delete from users;");
+    }
+
+    public User findById(String id) {
+        Map<String, String> env = System.getenv();
+        Connection c;
+        try {
+            c = dataSource.getConnection();
+
+            PreparedStatement pstmt = c.prepareStatement("SELECT * FROM users WHERE id = ?");
+            pstmt.setString(1, id);
+
+            ResultSet rs = pstmt.executeQuery();
+            User user = null;
+            if (rs.next()) {
+                user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+            }
+
+            rs.close();
+            pstmt.close();
+            c.close();
+
+            if (user == null) throw new RuntimeException();
+
+            return user;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ hello2/src/main/java/com/springboot/hello2/domain/dto/UserRequestDto.java
@@ -0,0 +1,4 @@
+package com.springboot.hello2.domain.dto;
+
+public class UserRequestDto {
+}
