72ac2467c843f1286db1daa650f913c7dabed5bc
lucyoz <lucyoz.kwon@gmail.com>
2022-10-28 11:34:37 +0900

    log.info 추가

--- hello2/src/main/java/com/springboot/hello2/controller/GetController.java
+++ hello2/src/main/java/com/springboot/hello2/controller/GetController.java
@@ -1,26 +1,31 @@
 package com.springboot.hello2.controller;
 
 import com.springboot.hello2.domain.dto.MemberDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController     //이게 붙어있어야 spring이 컨트롤러로 읽음
 @RequestMapping("/api/v1/get-api")
+@Slf4j
 public class GetController {
 
     @RequestMapping(value="/hello", method= RequestMethod.GET)
     public String hello(){
+        log.info("hello로 요총이 들어옴");
         return "Hello World";
     }
 
     @GetMapping(value="/name")
     public String getName(){
+        log.info("getName으로 요청이 들어옴");
         return "kwon";
     }
 
     @GetMapping(value="/variable1/{variable}")
     public String getVariable(@PathVariable String variable){
+        log.info("getVariable1으로 요청이 들어옴 variable:{}",variable);
         return variable;
     }
 
